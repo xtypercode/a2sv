be20bb3382d460d7eefbf20c0771cd1b1eb08742
Luís Carlos Baptista <88588157+xtypercode@users.noreply.github.com>
2026-03-10 21:38:51 +0100

    Add solution for Maximum Average Subarray I (trial 3)

---
+++ leetcode/maximum-average-subarray-i_trial3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        window_sum = sum(nums[:k])
+        max_sum = window_sum
+
+        for i in range(k, len(nums)):
+            window_sum += nums[i]
+            window_sum -= nums[i-k]
+
+            max_sum = max(max_sum, window_sum)
+
+        return max_sum / k
