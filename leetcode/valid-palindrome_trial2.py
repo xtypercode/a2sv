98b4762f28401486106b93f7b92c4d46f91687d9
Luís Carlos Baptista <88588157+xtypercode@users.noreply.github.com>
2026-03-14 20:30:51 +0100

    Add solution for Valid Palindrome (trial 2)

---
+++ leetcode/valid-palindrome_trial2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join([char for char in s if char.isalnum()]).lower()
+
+        l, r = 0, len(s)-1
+        while(l < r):
+            if(s[l] != s[r]):
+                return False
+            
+            l += 1
+            r -= 1
+
+        return True 
