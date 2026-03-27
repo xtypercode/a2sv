2dfcec6fe5d546904016475358b4927e138b77d7
Luís Carlos Baptista <88588157+xtypercode@users.noreply.github.com>
2026-03-27 17:03:51 +0100

    Add solution for Rotate Array (trial 1)

---
+++ leetcode/rotate-array_trial1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        k %= len(nums)
+
+        nums.reverse()
+        nums[:k] = reversed(nums[:k])
+        nums[k:] = reversed(nums[k:])
