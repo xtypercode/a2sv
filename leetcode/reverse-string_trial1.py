38dfd882a3f5f72da4f6d2e9a104069669bd360f
Luís Carlos Baptista <88588157+xtypercode@users.noreply.github.com>
2026-04-22 20:09:11 +0100

    Add solution for Reverse String (trial 1)

--- leetcode/reverse-string_trial1.py
+++ leetcode/reverse-string_trial1.py
@@ -1,13 +1,9 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        self.helper(s, 0, len(s)-1)
+        l, r = 0, len(s)-1
 
-    def helper(self, s: List[str], l: int, r: int) -> None:
-        if l >= r:
-            return
-        s[l], s[r] = s[r], s[l]
-        return self.helper(s, l + 1, r - 1)
+        while(l <= r):
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
         
