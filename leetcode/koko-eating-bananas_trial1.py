f93b12d1958f82add57108b95e7233a0ffc51a58
Luís Carlos Baptista <88588157+xtypercode@users.noreply.github.com>
2026-04-24 16:52:41 +0100

    Add solution for Koko Eating Bananas (trial 1)

---
+++ leetcode/koko-eating-bananas_trial1.py
@@ -0,0 +1,21 @@
+import math
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        res = r 
+        
+        while l <= r:
+            k = (l + r) // 2
+            total_time = 0
+            
+            for p in piles:
+                total_time += math.ceil(p / k)
+            
+            if total_time <= h:
+                res = k
+                r = k - 1
+            else:
+                l = k + 1
+                
+        return res
